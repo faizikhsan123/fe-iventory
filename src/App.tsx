5099c74145cb22913e6898befa7792aa2ac816c3
unknown <faizashary19@gmail.com>
2026-08-28 15:03:54 +0700

    add react hook form, zod, axios, react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      
-      
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <h1 className="text-2xl font-bold text-blue-600 underline">
+          Tailwind Berhasil Dipasang!
+        </h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
