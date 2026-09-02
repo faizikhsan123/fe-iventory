fa1245aa2cca79d5a2f54ab64510ab1d74283370
faiz ikhsan <faizashary19@gmail.com>
2026-09-02 16:22:44 +0700

    add card,sidebar,table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { Route, Routes } from "react-router";
+import Dashboard from "./pages/dashboard";
+import Items from "./pages/Items";
+import Supplier from "./pages/Supplier";
+import Employes from "./pages/Employes";
 
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+        <Routes>
+          <Route path="/" element={<Dashboard></Dashboard>}>dahsb</Route>
+          <Route path="/items" element={<Items></Items>}>items</Route>
+          <Route path="/supplier" element={<Supplier></Supplier>}>supplier</Route>
+          <Route path="/employes" element={<Employes></Employes>}>employes</Route>
+        </Routes>
+    </div>
   );
 };
 
